2cc68a26c103e5c33be6832e7763f117142fe57d
JC_dev <juancarlostorresmiranda96@gmail.com>
2024-03-12 10:30:22 -0500

    :Encabezado listo

--- Logica.js
+++ Logica.js
@@ -3,27 +3,33 @@ $(document).ready(function(){
     var img1 = $("#img1");
     var img2 = $("#img2");
     var img3 = $("#img3");
+    var imL =$("#imL");
+    var linea=$(".linea");
 
     var angle = 0;
 
-    // Asignar eventos de clic a las imágenes
+    // eventos de clic a las imágenes
     img1.click(function(){
-        angle = 92; // Cambiar el ángulo a 128 grados
+        angle = 92; // Cambiar el ángulo a  92 grados
         slider.css('transform', 'rotate(' + angle + 'deg)');
         console.log("im1")
+        imL.attr("src", "./assets/mostrito_L.png").fadeIn(1000);
+        linea.css("left","0");
     });
 
     img2.click(function(){
-        angle = 206; // Cambiar el ángulo a 128 grados
+        angle = 206; 
         slider.css('transform', 'rotate(' + angle + 'deg)');
         console.log("im2")
-
+        imL.attr("src", "./assets/chaufa_L.png").fadeIn(1000);
+        linea.css("left","159px");
     });
 
     img3.click(function(){
-        angle = 326; // Cambiar el ángulo a 128 grados
+        angle = 326; 
         slider.css('transform', 'rotate(' + angle + 'deg)');
         console.log("im3")
-
+        imL.attr("src", "./assets/pepino_L (1).png").fadeIn(1000);
+        linea.css("left","328px");
     });
 });
